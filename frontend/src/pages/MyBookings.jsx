2773fe09998b4aac9769af4bcc3ecc25e6305463
Nishanta Khadka <nisxant69@gmail.com>
2025-07-04 08:13:10 +0545

    Fix import statement for timeFormat utility in MyBookings component

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -3,7 +3,7 @@ import { dummyBookingData } from '../assets/assets'
 import Loading from '../components/Loading'
 import BlurCircle from '../components/BlurCircle'
 import { formatDateTime } from '../lib/formatTime'
-import { timeFormat } from '../lib/timeFormat'
+import timeFormat from '../lib/timeFormat'
 
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
